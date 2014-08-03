382cab838cd974dbae3e9d5c5f09ffe0f5045b7a
heliogabal <rhalbmann@gmail.com>
2014-08-03 15:36:35 +0200

    Update default.settings.php

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -343,7 +343,7 @@ ini_set('session.cookie_lifetime', 2000000);
 # $conf['site_name'] = 'My Drupal site';
 # $conf['theme_default'] = 'garland';
 # $conf['anonymous'] = 'Visitor';
-$conf['theme_default'] = 'nhahang';
+$conf['theme_default'] = 'nhahang_theme';
 
 /**
  * A custom theme can be set for the offline page. This applies when the site
