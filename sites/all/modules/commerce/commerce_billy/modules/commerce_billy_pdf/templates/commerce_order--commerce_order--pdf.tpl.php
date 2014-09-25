6a5a5b1d9d3aec212919f37056f3a3961df98011
jean <jean@pc-jean.(none)>
2014-09-25 22:31:43 +0200

    edit modules billy mail for email translation + nhahang info

--- sites/all/modules/commerce/commerce_billy/modules/commerce_billy_pdf/templates/commerce_order--commerce_order--pdf.tpl.php
+++ sites/all/modules/commerce/commerce_billy/modules/commerce_billy_pdf/templates/commerce_order--commerce_order--pdf.tpl.php
@@ -3,9 +3,6 @@
 /**
  * @file
  * Template for invoiced orders.
- */
-
-?>
 
 <div class="invoice-invoiced">
   <div class="header">
@@ -29,4 +26,76 @@
   <div class="invoice-text"><?php print render($content['invoice_text']); ?></div>
 
   <div class="invoice-footer"><?php print render($content['invoice_footer']); ?></div>
+</div>
+
+
+
+
+
+
+ */
+
+?>
+
+
+
+
+<?php
+
+
+
+
+/** 
+
+print_r(get_defined_vars());
+ * @file
+ * Template for invoiced orders.
+ */
+
+?>
+
+<div class="invoice-invoiced">
+  <div class="header">
+    <img src="<?php print $content['invoice_logo']['#value']; ?>"/>
+
+	<div class ="invoice-title">
+	Gundara<br>
+	</div>
+
+	<div class ="invoice-site">
+	<?php print t('http://gundara.com'); ?><br>
+	</div>
+
+	<div class="invoice-header">
+        <?php print render($content['invoice_header']); ?>  
+	</div>
+  </div>
+
+
+  <div class="invoice-header-date"><br><?php print render($content['invoice_header_date']); ?>
+	<b><i><?php print '<br>- ' . render($content['order_number']) . ' -'; ?></b></i>
+  </div>
+
+	
+  <div class="customer"><b><?php print t('Billing Info:'); ?></b><br /><?php print render($content['commerce_customer_billing']); ?></div>
+
+  <div class="customer_shipping"><br><b><?php print t('Shipping Address:'); ?></b><br /><?php print render($content['commerce_customer_shipping']); ?></div>
+
+
+
+  <div class="line-items">
+    <div class="line-items-view"><?php print render($content['commerce_line_items']); ?></div>
+    <div class="order-total"><?php print render($content['commerce_order_total']); ?></div>
+  </div>
+
+  <div class="invoice-text"><?php print render($content['invoice_text']); ?></div>
+
+  <div class="invoice-footer"><b>-    <?php print t('Thank you for being our client and for paying this bill within 15 days following the order date'); ?>    -</b></i><br><br><?php print render($content['invoice_footer']); ?>
+
+
+<b><?php print t('Bank Details'); ?>: </b>Volksbank Göttingen / Gunda Wiegmann / <i><?php print t('Bank Ref.'); ?></i>: 26090050  / <i><?php print t('Account Number'); ?></i>: 0104857001 / 
+<i>BIC</i>: GENODEF1GOE / <br><i>IBAN</i>: DE64260900500104857001 / <i><?php print t('Bank Address'); ?> </i>: Volksbank Göttingen eG / Kurze-Geismar-Straße 2 / 37073 Göttingen / <?php print t('Germany'); ?> <br>
+		<b>Paypal:</b>gundara@gundara.com<br></div>
+
+
 </div>
